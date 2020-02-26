07d00c96194f1673c54c68512a3331b8c7b828c6
luke williams <jluke94williams@gmail.com>
2020-02-26 17:30:52 -0500

    Added Local Strorage Script

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,87 +1,123 @@
 $(document).ready(function() {
-// Added Current Date to Header
-    var date = new Date();
-    var months=["JAN","FEB","MAR","APR","MAY","JUN","JUL","AUG","SEP","OCT","NOV","DEC"];
-    var todaysDate =date.getDate()+" "+months[date.getMonth()]+" "+date.getFullYear();
-    console.log(todaysDate);
+
+    // Added Current Date and Time to Jumbotron
+    var todaysDate = moment().format("MMMM Do YYYY, h:mm a");
     $("#currentDay").text(todaysDate);
 
-    var saveIcon = "assets/images/save-icon.png";
+    //Variable setting clock to 24 hour time
+    var current24Hour = moment().format('H');
 
-    var scheduledPlans = JSON.parse(localStorage.getItem("savedPlans"));
+    var myPlans = JSON.parse(localStorage.getItem("savedPlans"));
 
-    //if (scheduledPlans !== null) {
-        //plansArray = storedPlans;
-        //} else {
-       //plansArray = new Array(9);
-       //plansArray[4] = "test";
-    //};
+    activityArray = myPlans
+    
 
-
+    //Empties Div with Id Day Planner before for loop is run.
     var dayPlanner = $("#dayPlanner");
     dayPlanner.empty();
 
+
+
+    //For Loop setting variable i equal to hours day planner is avaliable.
     for (var i = 9; i<= 17; i++) {
-        // Start Time of Day 
-        index = 0;
-        rowDiv = $("<div>");
-        rowDiv.addClass("row");
-        rowDiv.attr("index", i)
 
-        timeDiv = $("<div>");
-        timeDiv.addClass("col-md-2");
+    //Set Identifier equal to index in array
+    var identifier = i - 9
 
-        timeSpan = $("<span>");
-        timeSpan.attr("class", "hour");
+    // Create Row for Each Hour In For Loop
+    rowDiv = $("<div>");
+    rowDiv.addClass("row");
+    rowDiv.attr("index", i)
 
-        var displayTime = 0;
-        var morningAfternoon = "";
-        if(i > 12) {
-            displayTime = i - 12;
-            morningAfternoon = "pm";
-        } else {
-            displayTime = i;
-            morningAfternoon = "am";
-        }
+    // Column 1 - Display Hour of the Day on Each Row
+    timeDiv = $("<div>");
+    timeDiv.addClass("col-md-2");
+    timeSpan = $("<span>");
+    timeSpan.attr("class", "hour");
 
-        timeSpan.text(displayTime + morningAfternoon);
+    // If Statements for AM and PM Hours
+    var displayTime = 0;
+    var morningAfternoon = "";
+    if(i > 12) {
+        displayTime = i - 12;
+        morningAfternoon = "pm";
+    } else {
+        displayTime = i;
+        morningAfternoon = "am";
 
-        rowDiv.append(timeDiv);
-        timeDiv.append(timeSpan);
-        //End Time of Day 
+    };
 
-        //Start Activity Input
-        activitySpan = $("<input>");
-        activitySpan.attr("id", "input-" + index);
-        activitySpan.attr("index", index);
-        activitySpan.attr("type", "text");
-        activitySpan.attr("class", "activity-input");
+    // Add Hour to Each Row
+    timeSpan.text(displayTime + morningAfternoon);
 
-        //activitySpan.val(plansArray[index])
+    // Append Display Hour to DOM Column 1
+    rowDiv.append(timeDiv);
+    timeDiv.append(timeSpan);
 
-        activityInpDiv = $("<div>");
-        activityInpDiv.addClass("col-md-8")
-        rowDiv.append(activityInpDiv);
-        activityInpDiv.append(activitySpan);
-        //End Activity Input
-        
-        let saveDiv = $('<div>');
-        saveDiv.addClass('col-md-2');
+    // Column 2 - Display Input
+    activityInpDiv = $("<div>");
+    activityInpDiv.addClass("col-md-8")
+    activitySpan = $("<input>");
+    activitySpan.attr("id", "input-" + identifier);
+    activitySpan.attr("index", identifier);
+    activitySpan.attr("type", "text");
+    activitySpan.attr("class", "activity-input");
 
-        let saveBtn = $('<i>');
-        saveBtn.attr('id', "saveid-" +index);
-        saveBtn.attr('save-id',index);
-        saveBtn.attr('class',"far fa-save saveIcon");
+    // Return Activity from Local Storage Array
+    activitySpan.val(activityArray [identifier]);
+
+    //Append Input to DOM Column 2
+    rowDiv.append(activityInpDiv);
+    activityInpDiv.append(activitySpan);
+
+    //Column 3 - Display Save Button       
+    var saveDiv = $('<div>');
+    saveDiv.addClass('col-md-2');
+    var saveBtn = $('<i>');
+    saveBtn.attr('id', "saveid-" + identifier);
+    saveBtn.attr('save-id', identifier);
+
+    //Utalized Bootstrap fa-sav for Save Button Icon
+    saveBtn.attr('class',"far fa-save saveBtn");
+
+    //Append Save Button to DOM Column 3
+    rowDiv.append(saveDiv);
+    saveDiv.append(saveBtn);
+
+    //Append Column 1, Column 2 and Column 3 to Day Planner Div
+    dayPlanner.append(rowDiv);
+
+    //Set Input Color Change to changes in time of day
+    if (parseInt(current24Hour) === i) {
+        activitySpan.attr("style", "background-color: #ff6961; color: white;");
+    } else if (parseInt(current24Hour) < i) {
+        activitySpan.attr("style", "background-color: #77dd77; color: white;");
+    } else if (parseInt(current24Hour) > i) {
+        activitySpan.attr("style", "background-color: #d3d3d3;")};
     
-       
-        rowDiv.append(saveDiv);
-        saveDiv.append(saveBtn);
+    };
 
-        dayPlanner.append(rowDiv);
+    // Click Event for Save Button
+    $(document).on("click","i" ,function(event) {                                                      
+        event.preventDefault();
+    //Identifies which Save Button was clicked with $(this)                                                   
+        var saveIdNum = $(this).attr("save-id");                                
+        var inputId = "#input-"+saveIdNum;                                       
+        var activityValue = $(inputId).val();    
+        console.log(saveIdNum);
+        console.log(inputId);     
+        console.log(activityValue);                    
 
-    }
+    //Stores the input value corresponding to clicked save button to Activity Array                                   
+        activityArray[saveIdNum] = activityValue;                            
+    //Saves Activity Array in Local Storage                                                   
+        localStorage.setItem("savedPlans", JSON.stringify(activityArray));
+        console.log(activityArray)
+    });        
 
-
+});                                                        
 
  
-});
+
+   
+
